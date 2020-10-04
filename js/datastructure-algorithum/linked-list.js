502b61c7fdf5293e71af7d11f7f58edbedfea414
Abdel Zaki <AZA@evac-train.com>
2020-10-04 11:36:21 +0200

    remove function added

--- js/datastructure-algorithum/linked-list.js
+++ js/datastructure-algorithum/linked-list.js
@@ -1,38 +1,56 @@
-// linked list
+/ linked list
 class linkedList{
 
-constructor(){
-    this.head = null;
-    this.tail= null;
-}
-
-append(value){
-    const element = {
-        value : value, next : null
-    }
-    
-    if (this.tail){
-        this.tail.next = element;
+    constructor(){
+        this.head = null;
+        this.tail= null;
     }
 
-    this.tail = element;
-    if(!this.head){
-        this.head = element;
-    }
-}
-toArray(){
-    const allElements = [];
-    let currentElement = this.head;
-    while(currentElement){
-         allElements.push(currentElement);
-         currentElement = currentElement.next;
-    }
-    return allElements;
-}
+    append(value){
+        const element = {
+            value : value, next : null
+        }
+        
+        if (this.tail){
+            this.tail.next = element;
+        }
 
+        this.tail = element;
+        if(!this.head){
+            this.head = element;
+        }
+    }
+
+    remove(){
+        if (!this.head) return;
+        let currentElement = this.head;
+        while(currentElement){
+            if(currentElement.next == this.tail){
+                currentElement.next = null;
+                this.tail = currentElement;
+            }
+            currentElement = currentElement.next;
+        }
+       if(this.head = this.tail){
+        this.head = null;
+        this.tail = null;
+       } 
+    }
+
+    toArray(){
+        const allElements = [];
+        let currentElement = this.head;
+        while(currentElement){
+            allElements.push(currentElement);
+            currentElement = currentElement.next;
+        }
+        console.log(allElements);
+    }
 }
 
 list1 = new linkedList();
 list1.append(1);
 list1.append(2);
+list1.toArray();
+list1.remove();
 list1.toArray();
