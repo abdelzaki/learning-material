30d959b6c366668c614cbe74d92dbf09c363a46e
Abdel <AZA@evac-train.com>
2020-10-09 13:28:46 +0200

    Graph Traverse

--- js/datastructure-algorithum/graph.js
+++ js/datastructure-algorithum/graph.js
@@ -29,18 +29,50 @@ class Graph{
          delete this.elements[element];
          return this;
     }
+
+    traverGraphRecursive(startingElement){
+        if(!this.elements[startingElement]) return;
+        const visited = [];
+        const traverseGraphRecur = (element) => {
+            console.log(element)
+            if(!visited.includes(element)){
+                visited.push(element);
+                this.elements.forEach(ele=>{
+                    traverseGraphRecur(ele)
+                })
+            }
+        }
+        traverseGraphRecur(startingElement);
+        return visited;
+    }
 }
 
-graph = new Graph;
-graph.addElement("cairo")
-graph.addElement("alex")
-graph.addElement("new York")
-graph.addElement("berlin")
-graph.addElement("wedel")
-graph.addRelation("wedel","alex");
-graph.addRelation("cairo","alex");
-graph.addRelation("new York","alex");
-graph.addRelation("cairo","new York");
-graph.addRelation("cairo","berlin");
-graph.removeElement("cairo");
-graph.removeElement("new York");
+// graph = new Graph;
+// graph.addElement("cairo")
+// graph.addElement("alex")
+// graph.addElement("new York")
+// graph.addElement("berlin")
+// graph.addElement("wedel")
+// graph.addRelation("wedel","alex");
+// graph.addRelation("cairo","alex");
+// graph.addRelation("new York","alex");
+// graph.addRelation("cairo","new York");
+// graph.addRelation("cairo","berlin");
+// graph.removeElement("cairo");
+// graph.removeElement("new York");
+
+graph2 = new Graph;
+graph2.addElement("A");
+graph2.addElement("B");
+graph2.addElement("C");
+graph2.addElement("D");
+graph2.addElement("E");
+graph2.addElement("F");
+graph2.addRelation("A","C");
+graph2.addRelation("A","B");
+graph2.addRelation("B","D");
+graph2.addRelation("C","E");
+graph2.addRelation("D","E");
+graph2.addRelation("E","F");
+graph2.addRelation("F","F");
+graph2.traverGraphRecursive("A")
